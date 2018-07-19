e10c90cba90f1b57878818a6933bdff66bb1000b
silvestrevivo <silvestrevivomillan@gmail.com>
2018-07-19 22:16:52 +0200

    adjusts in setting point

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,20 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import img from '../../images/webpack-logo.svg';
 
-const App = () => (
-  <div className="container">
-    <h1>Webpack4-React Starter</h1>
-    <img src={img} alt="weback-logo" />
-    <footer>
-      <small>
-        Developed by{' '}
-        <a href="https://github.com/silvestrevivo" target="_blank" rel="noreferrer noopener">
-          @silvestrevivo
-        </a>
-      </small>
-    </footer>
-  </div>
-);
+class App extends Component {
+  state = {};
+
+  render() {
+    return <div>React Mobiquity</div>;
+  }
+}
 
 export default hot(module)(App);
